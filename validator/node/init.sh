5639f7abaf0d5612f2726939949020561e39c173
Artem Eger <ae@atr0x.com>
2020-09-04 12:11:20 +0200

    Initial commit

---
+++ validator/node/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while getopts "w:" opt
+do
+   case "$opt" in
+      w ) wallet="$OPTARG" ;;
+   esac
+done
+
+printf "$wallet\n$wallet\n$wallet\n" | switcheocli keys import --keyring-backend file val_key /root/.key/val_key
+
+WALLET_PASSWORD=$wallet switcheod start
