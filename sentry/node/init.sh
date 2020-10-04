972101916cf7437eb457549f97c4a2e81559c4c9
Artem Eger <ae@atr0x.com>
2020-10-04 12:18:56 +0200

    prepare cosmovisor

--- sentry/node/init.sh
+++ sentry/node/init.sh
@@ -9,4 +9,8 @@ done
 
 printf "$wallet\n$wallet\n$wallet\n" | switcheocli keys import --keyring-backend file val_key /root/.key/val_key
 
-WALLET_PASSWORD=$wallet switcheod start --all
+export DAEMON_NAME=switcheod
+export DAEMON_HOME=${HOME}/.switcheod
+export PATH=${HOME}/.switcheod/cosmovisor/current/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
+WALLET_PASSWORD=$wallet cosmovisor start-all -a
