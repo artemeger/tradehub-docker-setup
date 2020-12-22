9260584e5f42a9ab45b2f0c6a7f2a08da0f22417
yuomii <artem-eger@web.de>
2020-12-22 14:00:48 +0100

    Add stalling protection

--- sentry/node/init.sh
+++ sentry/node/init.sh
@@ -13,4 +13,60 @@ export DAEMON_NAME=switcheod
 export DAEMON_HOME=${HOME}/.switcheod
 export PATH=${HOME}/.switcheod/cosmovisor/current/bin:${HOME}/.switcheod/cosmovisor/genesis/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
-WALLET_PASSWORD=$wallet cosmovisor start-all -a
+# Start oracle
+WALLET_PASSWORD=$wallet bash -c "exec -a oracle_process switcheod start --oracle --exclude=\"db,cosmos-rest\" --exclude-node &"
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start oracle_process: $status"
+  exit $status
+fi
+
+# Start persistence
+WALLET_PASSWORD=$wallet bash -c "exec -a persistence_process switcheod start --db --exclude-node &"
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start persistence_process: $status"
+  exit $status
+fi
+
+# Start cosmovisor
+WALLET_PASSWORD=$wallet bash -c "exec -a cosmovisor_process cosmovisor start -a --exclude=\"oracle,db\" &"
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start cosmovisor_process: $status"
+  exit $status
+fi
+
+old_height=0
+counter=0
+
+while sleep 5; do
+
+  echo "Checking processes..."
+  ps aux |grep oracle_process |grep -q -v grep
+  PROCESS_1_STATUS=$?
+  ps aux |grep persistence_process |grep -q -v grep
+  PROCESS_2_STATUS=$?
+  ps aux |grep cosmovisor_process |grep -q -v grep
+  PROCESS_3_STATUS=$?
+  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 -o $PROCESS_3_STATUS -ne 0 ]; then
+    echo "One of the processes exited. Abort"
+    exit 1
+  fi
+
+  echo "Checking stalling..."
+  local_height=$(curl -s "http://localhost:26659/status" | jq -r '.result.sync_info.latest_block_height')
+  if [ $local_height -ne $old_height ]; then
+		echo "Blockheight increasing - OK: $(($local_height-$old_height)) Counter: $counter"
+		counter=$((0))
+		old_height=$((local_height))
+	else
+		counter=$((counter+1))
+		echo "Blockheight stalled - BAD: $(($local_height-$old_height)) Counter: $counter"
+	fi
+	if [ $counter -gt 20 ]; then
+	  echo "Node seems stalled. Abort"
+		exit 1
+	fi
+
+done
--- validator/node/init.sh
+++ validator/node/init.sh
@@ -13,4 +13,40 @@ export DAEMON_NAME=switcheod
 export DAEMON_HOME=${HOME}/.switcheod
 export PATH=${HOME}/.switcheod/cosmovisor/current/bin:${HOME}/.switcheod/cosmovisor/genesis/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
-WALLET_PASSWORD=$wallet cosmovisor start-all
+# Start validator
+WALLET_PASSWORD=$wallet bash -c "exec -a validator_process cosmovisor start-all &"
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start validator_process: $status"
+  exit $status
+fi
+
+old_height=0
+counter=0
+
+while sleep 5; do
+
+  echo "Checking validator process..."
+  ps aux |grep validator_process |grep -q -v grep
+  PROCESS_1_STATUS=$?
+  if [ $PROCESS_1_STATUS -ne 0 ]; then
+    echo "Validator process exited. Abort"
+    exit 1
+  fi
+
+  echo "Checking stalling..."
+  local_height=$(curl -s "http://localhost:26657/status" | jq -r '.result.sync_info.latest_block_height')
+  if [ $local_height -ne $old_height ]; then
+		echo "Blockheight increasing - OK: $(($local_height-$old_height)) Counter: $counter"
+		counter=$((0))
+		old_height=$((local_height))
+	else
+		counter=$((counter+1))
+		echo "Blockheight stalled - BAD: $(($local_height-$old_height)) Counter: $counter"
+	fi
+	if [ $counter -gt 20 ]; then
+	  echo "Node seems stalled. Abort"
+		exit 1
+	fi
+
+done
